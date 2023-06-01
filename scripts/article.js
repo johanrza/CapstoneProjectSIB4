33c565ed075d57d984ccff1284dd711c026b03eb
Johan R <johanreza58@gmail.com>
2023-06-02 00:22:20 +0700

    accessibility button article

--- scripts/article.js
+++ scripts/article.js
@@ -7,11 +7,11 @@ newsData.news.forEach(({ image, title, description, newsUrl }) => {
   article.innerHTML += `
     <div class="col">
     <div class="card h-100">
-      <img src="${image}" class="card-img-top" alt="...">
+      <img src="${image}" class="card-img-top" alt="">
       <div class="card-body">
         <h5 class="card-title">${title}</h5>
         <p class="card-text">${description}</p>
-        <a href="${newsUrl}" class="btn px-0" target="_blank">Read More <i class="fa-solid fa-angles-right"></i></a>
+        <a class="icon-link icon-link-hover" style="--bs-link-hover-color-rgb: 25, 135, 84;" href="#">Read More <i class="bi fa-solid fa-angles-right"></i></a>
       </div>
     </div>
   </div>`;

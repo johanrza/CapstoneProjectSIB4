0ffeed3ee06bf839be18fa811b700a0b9fd145e9
Johan R <johanreza58@gmail.com>
2023-06-04 19:51:58 +0700

    improve news readmore

--- scripts/article.js
+++ scripts/article.js
@@ -8,10 +8,10 @@ newsData.news.forEach(({ image, title, description, newsUrl }) => {
     <div class="col">
     <div class="card h-100">
       <img src="${image}" class="card-img-top" alt="">
-      <div class="card-body">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">${title}</h5>
         <p class="card-text">${description}</p>
-        <a class="icon-link icon-link-hover" style="--bs-link-hover-color-rgb: 25, 135, 84;" href="${newsUrl}" target="_blank">Read More <i class="bi fa-solid fa-angles-right"></i></a>
+        <a class="icon-link icon-link-hover mt-auto" style="--bs-link-hover-color-rgb: 25, 135, 84;" href="${newsUrl}" target="_blank">Read More <i class="bi fa-solid fa-angles-right"></i></a>
       </div>
     </div>
   </div>`;

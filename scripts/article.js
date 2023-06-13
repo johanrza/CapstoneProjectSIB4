794ea391601f3819d07d3d91ba1f8cd27b17e522
Johan R <johanreza58@gmail.com>
2023-06-13 16:08:17 +0700

    create weather widget on the home page

--- scripts/article.js
+++ scripts/article.js
@@ -8,7 +8,7 @@ newsData.news.forEach(({ image, title, description, newsUrl }) => {
     <div class="col">
     <div class="card h-100">
       <img src="${image}" class="card-img-top" alt="">
-      <div class="card-body d-flex flex-column">
+      <div id="newsBody" class="card-body d-flex flex-column">
         <h5 class="card-title">${title}</h5>
         <p class="card-text">${description}</p>
         <a class="icon-link icon-link-hover mt-auto" style="--bs-link-hover-color-rgb: 25, 135, 84;" href="${newsUrl}" target="_blank">Read More <i class="bi fa-solid fa-angles-right"></i></a>
